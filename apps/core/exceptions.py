509c259fd27ab5f7e71c58e35e14591f657fed9e
alyahmady <better.aly.ahmady@gmail.com>
2023-02-01 09:49:13 +0330

    feat: add custom exception handler

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,5 +1,33 @@
+import traceback
+
+from django.conf import settings
 from rest_framework import status
 from rest_framework.exceptions import APIException
+from rest_framework.views import exception_handler
+
+from apps.core.responses import ErrorResponse
+
+
+def custom_exception_handler(exc: Exception, context: dict):
+    """Custom API exception handler"""
+
+    if isinstance(exc, CustomAPIException):
+        return ErrorResponse(
+            message=exc.error_message, data=exc.error_data, status=exc.status_code
+        )
+
+    response = exception_handler(exc, context)
+    if settings.DEBUG:
+        if isinstance(response.data, list):
+            response.data.append({"exception_detail": traceback.format_exc()})
+        elif isinstance(response.data, dict):
+            response.data.update({"exception_detail": traceback.format_exc()})
+        else:
+            response.data = {"exception_detail": traceback.format_exc()}
+
+    return ErrorResponse(
+        message="Unexpected Error", status=response.status_code, data=response.data
+    )
 
 
 class CustomAPIException(APIException):
--- config/settings/rest_framework.py
+++ config/settings/rest_framework.py
@@ -3,5 +3,5 @@ REST_FRAMEWORK = {
     #     "rest_framework_simplejwt.authentication.JWTAuthentication",
     # ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    # "EXCEPTION_HANDLER": "apps.core.exceptions.custom_exception_handler",
+    "EXCEPTION_HANDLER": "apps.core.exceptions.custom_exception_handler",
 }
