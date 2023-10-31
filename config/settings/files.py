e679e796caed7f12a8639842330831f3a2da2ec1
Hamid Reza Shakibi <h_shakibi@felixin.io>
2023-10-31 13:34:40 +0000

    feat: add template ci/cd files

--- config/settings/files.py
+++ config/settings/files.py
@@ -2,7 +2,10 @@ from .handler import BASE_DIR
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = env.str("PUBLIC_STATIC_ROOT", STATIC_ROOT) # This ENV variable will be used in the CI/CD
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
@@ -11,4 +14,5 @@ STATICFILES_FINDERS = [
 
 # MEDIA
 MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = env.str("PUBLIC_MEDIA_ROOT", MEDIA_ROOT) # This ENV variable will be used in the CI/CD
 MEDIA_URL = "/media/"
